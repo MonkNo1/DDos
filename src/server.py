71222d7340db6d6abb0cb9e6f06ff81c890e3036
MonkNo1 <hitman20025@gmail.com>
2023-10-11 01:23:05 +0530

    full server

--- src/server.py
+++ src/server.py
@@ -4,21 +4,18 @@ import time
 import ipaddress
 import json
 
-def logit(addr,threadCount):
+def logit(addr,threadCount,messg):
     with open("output/log.txt",'+a') as f :
-        content = f"[{time.ctime()}] :: connection from {addr} with {threadCount} threads"
+        content = f"[{time.ctime()}] :: connection from {addr} with {threadCount} threads :: {messg}"
         f.write(content)
         
 def get_publicIp():
     endpoint = 'https://ipinfo.io/json'
     response = requests.get(endpoint, verify = True)
-
     if response.status_code != 200:
         return 'Status:', response.status_code, 'Problem with the request. Exiting.'
         exit()
-
     data = response.json()
-
     return data['ip']
 
 s = socket.socket()
@@ -41,7 +38,20 @@ while True:
     c,addr   = s.accept()
     print(f"Got connection form {addr}!!")
     c.send("Enter the Thread Count For the attack 5-30 [default=10]: ".encode())
-    thread = c.recv(1024).decode()
-    
+    try:
+        thread = int(c.recv(1024).decode())
+        logit(addr=addr,threadCount=thread,messg="Connected")
+        c.send("The attack will be Started Press [ S ] to Continue  : ".encode())
+        confo = c.recv(1024).decode()
+        if(confo == 's' or confo == 'S'):
+            c.send("Started").encode()
+            logit(addr=addr,threadCount=thread,messg="Started")
+        else:
+            c.send("ITs Not Starting ....".encode())
+            c.send("Exiting......!".encode())
+            logit(addr=addr,threadCount=thread,messg="Exited")
+            s.close()
+    except:
+        pass
     
     
